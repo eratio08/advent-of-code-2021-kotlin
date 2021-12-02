6a83e836fbfb6400fce71718d8215812b311c91b
Eike Lurz <eike.lurz@tuta.io>
2021-12-02 15:09:10 +0100

    Add gradle task generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,3 +23,14 @@ tasks {
         gradleVersion = "7.3"
     }
 }
+
+fileTree("${project.projectDir}")
+    .matching { include("**/*.kt") }
+    .asSequence()
+    .filter { it.name.contains("Day") }
+    .forEach {
+        tasks.register<JavaExec>(it.name.substringBefore(".kt")) {
+            classpath = sourceSets.main.get().runtimeClasspath
+            mainClass.set(it.name.replace(".kt", "Kt"))
+        }
+    }
